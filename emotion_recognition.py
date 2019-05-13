6c7d1aeedacc627d385567323a34804eb2a0aed8
matrousseau <matthieur.rousseau@gmail.com>
2019-05-13 13:21:00 +0200

    API cognitive

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -9,32 +9,31 @@ from io import BytesIO
 class EmotionCapture():
 
     def __init__(self):
-        self.flow = cv2.VideoCapture(1)
-        self.subscription_key = "88c9982696ac4d848b775ff3e43ffe7e"
-        self.vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
-        self.analyze_url = self.vision_base_url + "analyze"
-        assert self.subscription_key
+        self.flow = cv2.VideoCapture(0)
+
 
 
     def detect_emotion(self, count):
 
-        subscription_key = "88c9982696ac4d848b775ff3e43ffe7e"
+        subscription_key = "bbe9aa125b48403c9c6190284d593b93"
         assert subscription_key
 
-        vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+        face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
 
-        analyze_url = vision_base_url + "analyze"
 
         # Set image_path to the local path of an image that you want to analyze.
         image_path = "D:\\IA\\Projets\\Multi-Faces-Emotion-Detection-in-real-time-using-Azure\\13-05-19\\frame%d.jpg" % count
 
         # Read the image into a byte array
         image_data = open(image_path, "rb").read()
-        headers    = {'Ocp-Apim-Subscription-Key': subscription_key,
-                      'Content-Type': 'application/octet-stream'}
-        params     = {'visualFeatures': 'Categories,Description,Color'}
-        response = requests.post(
-            analyze_url, headers=headers, params=params, data=image_data)
+        headers = { 'Ocp-Apim-Subscription-Key': subscription_key }
+
+        params = {
+            'returnFaceId': 'true',
+            'returnFaceLandmarks': 'false',
+            'returnFaceAttributes': 'age,gender,emotion',
+        }
+        response = requests.post(face_api_url, headers=headers, params=params, data=image_data)
         response.raise_for_status()
 
         # The 'analysis' object contains various fields that describe the image. The most
@@ -50,7 +49,6 @@ class EmotionCapture():
             ret, frame = self.flow.read()
             cv2.imwrite("D:\\IA\\Projets\\Multi-Faces-Emotion-Detection-in-real-time-using-Azure\\13-05-19\\frame%d.jpg" % count, frame)
             self.detect_emotion(count)
-            count +=1
             # Display the resulting frame
             cv2.imshow('frame',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
