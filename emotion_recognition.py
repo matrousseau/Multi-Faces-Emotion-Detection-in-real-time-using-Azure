e4fcf38eba6671e1d3a28e3a1e2812841ab08705
matrousseau <matthieur.rousseau@gmail.com>
2019-05-13 13:53:17 +0200

    API cognitive

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -5,42 +5,42 @@ import requests
 import matplotlib.pyplot as plt
 from PIL import Image
 from io import BytesIO
+import os
+import datetime
+import json
+
 
 class EmotionCapture():
 
     def __init__(self):
         self.flow = cv2.VideoCapture(0)
+        self.subscription_key = "bbe9aa125b48403c9c6190284d593b93"
+        assert self.subscription_key
+        self.face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
 
-
+        now = datetime.datetime.now()
+        self.path_folder = now.strftime("%Y-%m-%d-%Hh%Mm%Ss")
+        os.mkdir(self.path_folder)
+        os.mkdir(self.path_folder + '/logs')
+        os.mkdir(self.path_folder + '/img')
 
     def detect_emotion(self, count):
 
-        subscription_key = "bbe9aa125b48403c9c6190284d593b93"
-        assert subscription_key
+        image_path = self.path_folder + "/img/frame%d.jpg" % count
 
-        face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
-
-
-        # Set image_path to the local path of an image that you want to analyze.
-        image_path = "D:\\IA\\Projets\\Multi-Faces-Emotion-Detection-in-real-time-using-Azure\\13-05-19\\frame%d.jpg" % count
-
-        # Read the image into a byte array
         image_data = open(image_path, "rb")
-        headers = {'Content-Type': 'application/octet-stream',
-                    'Ocp-Apim-Subscription-Key': subscription_key}
-
+        headers = {'Content-Type': 'application/octet-stream','Ocp-Apim-Subscription-Key': self.subscription_key}
         params = {
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false',
-            'returnFaceAttributes': 'age,gender,emotion',
-        }
-        response = requests.post(face_api_url, headers=headers, params=params, data=image_data)
+            'returnFaceAttributes': 'age,gender,emotion'}
+
+        response = requests.post(self.face_api_url, headers=headers, params=params, data=image_data)
         response.raise_for_status()
 
-        # The 'analysis' object contains various fields that describe the image. The most
-        # relevant caption for the image is obtained from the 'description' property.
         analysis = response.json()
-        print(analysis)
+        with open(self.path_folder + "/logs/logs%d.json" % count, 'w') as outfile:
+            json.dump(analysis, outfile)
 
 
     def capture_frame(self):
@@ -48,12 +48,14 @@ class EmotionCapture():
         while(True):
             # Capture frame-by-frame
             ret, frame = self.flow.read()
-            cv2.imwrite("D:\\IA\\Projets\\Multi-Faces-Emotion-Detection-in-real-time-using-Azure\\13-05-19\\frame%d.jpg" % count, frame)
+            cv2.imwrite(self.path_folder + "/img/frame%d.jpg" % count, frame)
             self.detect_emotion(count)
+            count += 1
             # Display the resulting frame
             cv2.imshow('frame',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
 
 
 
