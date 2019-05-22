655659953b10cd68d15816397e890db8e1fbbae6
matrousseau <matthieur.rousseau@gmail.com>
2019-05-22 14:33:00 +0200

    Basic Azure API

--- JsonManager.py
+++ JsonManager.py
@@ -1,12 +1,229 @@
-import json
+# une_variable = "Je suis une variable globale"
+#
+# def function1():
+#     global une_variable
+#
+#     une_variable = 2
+#
+#
+#
+# function1()
+# print(une_variable)
+#
+#
+# a = [1,2,3,4,5,6]
+# print(a[-1])
+#
+# import math
+#
+# throttle = 5
+# alpha = 0.5
+# total_progress = 0.6
+#
+# print(throttle/2 - (math.cos(alpha))/2 + 3*total_progress)
+#
+#
+# import math
+# #
+# # all_x = []
+# # all_y = []
+# #
+# # def reward_function(params):
+# #
+# #     global all_y
+# #     global all_x
+# #
+# #     MAX_SPEED = 5
+# #     reward = 1e-5
+# #     is_speed = 0
+# #
+# #     all_x.append(params["x"])
+# #     all_x.append(params["y"])
+# #     throttle = params['steering_angle']
+# #     x = params["x"]
+# #     y = params["y"]
+# #     total_progress = params['progress']
+# #
+# #     distance_from_center = params['distance_from_center']
+# #
+# #     try:
+# #         x_prev = all_x[-2]
+# #         y_prev = all_y[-2]
+# #     except:
+# #         x_prev = x
+# #         y_prev = y
+# #
+# #     alpha = get_alpha(x, y, x_prev, y_prev, distance_from_center)
+# #     alpha = 0
+# #     print('Alpha = ', alpha)
+# #     print('Len All_X', len(all_x))
+# #     print('Len All_Y', len(all_y))
+# #
+# #     track_width = params['track_width']
+# #     distance_from_center = params['distance_from_center']
+# #
+# #     reward = 4.5 * (1 - math.exp(abs(distance_from_center)))
+# #
+# #     if throttle > 0.9 * MAX_SPEED:
+# #         is_speed = 1
+# #
+# #     reward_cos = throttle * (math.cos(alpha)-max([1-(total_progress/100)-0.2, 0]))
+# #     # smooth = throttle * (math.cos(alpha) - 1/(1 + math.exp(-4*(abs(1-(total_progress/100))-0.2))))
+# #
+# #     reward += reward_cos + is_centered + is_speed
+# #
+# #     return float(reward)
+# #
+# # def get_alpha(x, y, x_prev, y_prev, distance_from_center):
+# #
+# #     res = 0
+# #     denominateur = 0
+# #     alpha = 0
+# #     numerateur = distance_from_center
+# #
+# #     denominateur = math.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev))
+# #
+# #     if denominateur > 0 and denominateur > numerateur :
+# #         res = np.arcsin(numerateur/denominateur)
+# #         self.alpha = res
+# #         if res == np.NaN :
+# #             return 0
+# #
+# #     return res
+# import math
+#
+# all_x = []
+# all_y = []
+#
+# def reward_function(params):
+#
+#     global all_y
+#     global all_x
+#
+#     MAX_SPEED = 5
+#     reward = 1e-5
+#     is_speed = 0
+#
+#     all_x.append(params["x"])
+#     all_x.append(params["y"])
+#     throttle = params['steering_angle']
+#     x = params["x"]
+#     y = params["y"]
+#     total_progress = params['progress']
+#
+#     distance_from_center = params['distance_from_center']
+#
+#     try:
+#         x_prev = all_x[-2]
+#         y_prev = all_y[-2]
+#     except:
+#         x_prev = x
+#         y_prev = y
+#
+#     alpha = get_alpha(x, y, x_prev, y_prev, distance_from_center)
+#     alpha = 0
+#     print('Alpha = ', alpha)
+#
+#     track_width = params['track_width']
+#     distance_from_center = params['distance_from_center']
+#
+#
+#     reward = -4.54 * abs(distance_from_center) + 1
+#
+#
+#     if throttle < 0.9 * MAX_SPEED:
+#         reward *= 0.8
+#
+#     reward *= throttle/2 - (math.cos(alpha))/2 + 3*total_progress
+#
+#     return float(reward)
+#
+# def get_alpha(x, y, x_prev, y_prev, distance_from_center):
+#
+#     res = 0
+#     denominateur = 0
+#     alpha = 0
+#     numerateur = distance_from_center
+#
+#     denominateur = math.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev))
+#
+#     if denominateur > 0 and denominateur > numerateur :
+#         res = np.arcsin(numerateur/denominateur)
+#         self.alpha = res
+#         if res == np.NaN :
+#             return 0
+#
+#     return res
+import math
 
-with open('D:\\IA\\Projets\\Multi-Faces-Emotion-Detection-in-real-time-using-Azure\\2019-05-14-10h02m33s\\logs\logs8.json') as json_file:
-    data = json.load(json_file)
-    top = data[0]['faceRectangle']['top']
-    left = data[0]['faceRectangle']['left']
-    width = data[0]['faceRectangle']['width']
-    height = data[0]['faceRectangle']['height']
-    
-    print(top, left, width, height)
+a = [1,2,3]
 
-    # for p in data[0]['faceRectangle']:
+print(math.mean(a))
+
+
+
+
+
+import math
+
+all_x = []
+all_y = []
+
+def reward_function(params):
+
+    global all_y
+    global all_x
+
+    MAX_SPEED = 5
+    reward = 1e-5
+    is_speed = 0
+
+    all_x.append(params["x"])
+    all_x.append(params["y"])
+    throttle = params['steering_angle']
+    x = params["x"]
+    y = params["y"]
+    total_progress = params['progress']
+
+    distance_from_center = params['distance_from_center']
+
+    try:
+        x_prev = all_x[-2]
+        y_prev = all_y[-2]
+    except:
+        x_prev = x
+        y_prev = y
+
+    alpha = get_alpha(x, y, x_prev, y_prev, distance_from_center)
+    print('Alpha = ', alpha)
+
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+
+
+    reward = 1.5*(-4.54 * abs(distance_from_center) + 1)
+
+
+    if throttle < 0.9 * MAX_SPEED:
+        reward *= 0.8
+
+    reward *= throttle/2 - (math.cos(alpha))/2 + 3*total_progress
+
+    return float(reward)
+
+def get_alpha(x, y, x_prev, y_prev, distance_from_center):
+
+    res = 0
+    denominateur = 0
+    alpha = 0
+    numerateur = distance_from_center
+
+    denominateur = math.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev))
+
+    if denominateur > 0 and denominateur > numerateur :
+        res = np.arcsin(numerateur/denominateur)
+        self.alpha = res
+        if res == np.NaN :
+            return 0
+
+    return res
--- emotion_recognition.py
+++ emotion_recognition.py
@@ -7,12 +7,13 @@ from PIL import Image
 import os
 import datetime
 import json
+import csv
 
 class EmotionCapture():
 
     def __init__(self):
         self.flow = cv2.VideoCapture(1)
-        self.subscription_key = "bbe9aa125b48403c9c6190284d593b93"
+        self.subscription_key = "e64dd3ce1fdf46a4b4c3094dddc8f9d8"
         assert self.subscription_key
         self.face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
         now = datetime.datetime.now()
@@ -20,6 +21,7 @@ class EmotionCapture():
         os.mkdir(self.path_folder)
         os.mkdir(self.path_folder + '/logs')
         os.mkdir(self.path_folder + '/img')
+        self.init = True
 
     def detect_emotion(self, frame, count):
 
@@ -31,17 +33,16 @@ class EmotionCapture():
         params = {
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false',
-            'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-            'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+            'returnFaceAttributes': 'age,gender,headPose,emotion'
         }
 
         response = requests.post(self.face_api_url, params=params, headers=headers, data=image_data)
         response.raise_for_status()
         faces = response.json()
         frame = self.add_square(frame, faces)
-
-        with open(self.path_folder + "/logs/logs%d.json" % count, 'w') as outfile:
-            json.dump(faces, outfile)
+        self.export_data(faces)
+        # with open(self.path_folder + "/logs/logs%d.json" % count, 'w') as outfile:
+        #     json.dump(faces, outfile)
 
         return frame
 
@@ -57,22 +58,51 @@ class EmotionCapture():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
+    def export_data(self, json):
+
+        liste_neutre = []
+        liste_surprise = []
+        liste_happiness = []
+        nb_homme = 0
+        nb_femme = 0
+        age = []
+
+        now = datetime.datetime.now()
+        now = str(now.hour)+':'+str(now.minute)+':'+str(now.second)
+
+        for face in json:
+            if face['faceAttributes']['gender'] == 'male':
+                nb_homme += 1
+            else:
+                nb_femme +=1
+            age.append(face['faceAttributes']['age'])
+            liste_neutre.append(face['faceAttributes']['emotion']['neutral'])
+            liste_surprise.append(face['faceAttributes']['emotion']['surprise'])
+            liste_happiness.append(face['faceAttributes']['emotion']['happiness'])
+
+        title = ['date','neutre','surprise','happiness','homme','femme','age']
+        final_array = np.asarray([now, float(np.mean(liste_neutre)), float(np.mean(liste_surprise)), float(np.mean(liste_happiness)), nb_homme, nb_femme, np.mean(age)])
+
+        if self.init :
+            with open('output.csv','a', newline='') as out:
+                spamwriter = csv.writer(out, delimiter=';')
+                spamwriter.writerow(title)
+                spamwriter.writerow(final_array)
+            self.init = False
+        else:
+            with open('output.csv','a', newline='') as out:
+                spamwriter = csv.writer(out, delimiter=';')
+                spamwriter.writerow(final_array)
+
     def add_square(self, image, faces):
-
-
         for face in faces:
             fr = face["faceRectangle"]
             fa = face["faceAttributes"]
-            print(fr)
             origin = (fr["left"], fr["top"])
-            print(origin, fr["width"], fr["height"])
             cv2.rectangle(image, (fr["left"], fr["top"]+fr["height"]), (fr["left"]+fr["width"], fr["top"]-fr["height"]),(0,255,0),3)
-            # cv2.putText(image, "%s, %d"%(fa["gender"].capitalize(), fa["age"]),fontsize=10, weight="regular", va="bottom")
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(image,"%s, %d"%(fa["gender"].capitalize(), fa["age"]),(fr["left"]+fr["width"], fr["top"]-fr["height"]), font, 2,(255,255,255),2,cv2.LINE_AA)
-
         return image
-
 
 
 capture = EmotionCapture()
