bea752665c1f4f93d794719d7a70950698f27014
paceart <pacearthur@gmail.com>
2019-05-27 16:15:20 +0200

    database export

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -7,13 +7,17 @@ from PIL import Image
 import os
 import datetime
 import json
-import csv
+import sqlite3
+
+conn = sqlite3.connect('faces.db')
+
+c = conn.cursor()
 
 class EmotionCapture():
 
     def __init__(self):
         self.flow = cv2.VideoCapture(1)
-        self.subscription_key = "e64dd3ce1fdf46a4b4c3094dddc8f9d8"
+        self.subscription_key = '763213624f1a4069804a5ef00f702e1b'
         assert self.subscription_key
         self.face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
         now = datetime.datetime.now()
@@ -21,7 +25,6 @@ class EmotionCapture():
         os.mkdir(self.path_folder)
         os.mkdir(self.path_folder + '/logs')
         os.mkdir(self.path_folder + '/img')
-        self.init = True
 
     def detect_emotion(self, frame, count):
 
@@ -33,16 +36,17 @@ class EmotionCapture():
         params = {
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false',
-            'returnFaceAttributes': 'age,gender,headPose,emotion'
+            'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+            'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
         }
 
         response = requests.post(self.face_api_url, params=params, headers=headers, data=image_data)
         response.raise_for_status()
         faces = response.json()
         frame = self.add_square(frame, faces)
-        self.export_data(faces)
-        # with open(self.path_folder + "/logs/logs%d.json" % count, 'w') as outfile:
-        #     json.dump(faces, outfile)
+
+        with open(self.path_folder + "/logs/logs%d.json" % count, 'w') as outfile:
+            json.dump(faces, outfile)
 
         return frame
 
@@ -57,52 +61,46 @@ class EmotionCapture():
             cv2.imshow('frame',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
-    def export_data(self, json):
-
-        liste_neutre = []
-        liste_surprise = []
-        liste_happiness = []
-        nb_homme = 0
-        nb_femme = 0
-        age = []
-
-        now = datetime.datetime.now()
-        now = str(now.hour)+':'+str(now.minute)+':'+str(now.second)
-
-        for face in json:
-            if face['faceAttributes']['gender'] == 'male':
-                nb_homme += 1
-            else:
-                nb_femme +=1
-            age.append(face['faceAttributes']['age'])
-            liste_neutre.append(face['faceAttributes']['emotion']['neutral'])
-            liste_surprise.append(face['faceAttributes']['emotion']['surprise'])
-            liste_happiness.append(face['faceAttributes']['emotion']['happiness'])
-
-        title = ['date','neutre','surprise','happiness','homme','femme','age']
-        final_array = np.asarray([now, float(np.mean(liste_neutre)), float(np.mean(liste_surprise)), float(np.mean(liste_happiness)), nb_homme, nb_femme, np.mean(age)])
-
-        if self.init :
-            with open('output.csv','a', newline='') as out:
-                spamwriter = csv.writer(out, delimiter=';')
-                spamwriter.writerow(title)
-                spamwriter.writerow(final_array)
-            self.init = False
-        else:
-            with open('output.csv','a', newline='') as out:
-                spamwriter = csv.writer(out, delimiter=';')
-                spamwriter.writerow(final_array)
-
+    
     def add_square(self, image, faces):
+
+
         for face in faces:
             fr = face["faceRectangle"]
             fa = face["faceAttributes"]
+            fe = fa["emotion"]
+
+            age = fa["age"]
+            gender = fa["gender"]
+
+            surprise = fe["surprise"]
+            neutral = fe["neutral"]
+            happiness = fe["happiness"]
+
+            # print(fr)
             origin = (fr["left"], fr["top"])
-            cv2.rectangle(image, (fr["left"], fr["top"]+fr["height"]), (fr["left"]+fr["width"], fr["top"]-fr["height"]),(0,255,0),3)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(image,"%s, %d"%(fa["gender"].capitalize(), fa["age"]),(fr["left"]+fr["width"], fr["top"]-fr["height"]), font, 2,(255,255,255),2,cv2.LINE_AA)
+            # print(origin, fr["width"], fr["height"])
+            cv2.rectangle(image, (fr["left"], fr["top"]+fr["height"]), (fr["left"]+fr["width"], fr["top"]),(253,253,253),2)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(image,"%s"%(gender.capitalize()),(fr["left"]+fr["width"], fr["top"]), font, 1,(255,255,255))
+            cv2.putText(image,"%s"%(age),(fr["left"]+fr["width"], fr["top"]+ 40), font, 1,(255,255,255))
+
+            
+            # if surprise > 0.5:
+            #     cv2.putText(image,"%s"%(("Surprise"),(fr["left"]+fr["width"], fr["top"] + 120), font, 1,(255,255,255))
+            # elif neutral > 0.5:
+            #     cv2.putText(image,"%s"%(("Neutre"),(fr["left"]+fr["width"], fr["top"] + 120), font, 1,(255,255,255))
+            # elif happiness > 0.5:
+            #     cv2.putText(image,"%s"%(("Happy"),(fr["left"]+fr["width"], fr["top"] + 120), font, 1,(255,255,255))
+
+
+            c.execute("INSERT INTO faces VALUES (?, ?, ?, ?, ?)", (gender, age, surprise, neutral, happiness))
+
+        conn.commit()
+        
+        
         return image
+
 
 
 capture = EmotionCapture()
